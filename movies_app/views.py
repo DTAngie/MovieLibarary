a6ab27cf34656ec88a8f89fac30f79b4e6880dd5
Angeline Teets <angie@angiemac.lan>
2020-12-10 22:52:53 -0800

    detail view

--- movies_app/views.py
+++ movies_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+
+
+# movies = [
+#   Movie('Matrix', 1999, 'Wachowski', 'Sci-Fi'),
+#   Movie('Klaus', 2019, 'Sergio Pablos', 'Holiday'),
+#   Movie('Toy Story', 1995, 'John Lassiter', 'Family')
+# ]
 
 def home(request):
     return HttpResponse('<h1>Hello</h1>')
@@ -8,4 +16,9 @@ def about(request):
     return render(request, 'about.html')
 
 def movies_index(request):
+    movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+def movies_detail(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
