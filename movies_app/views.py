40741c76db7dcad2d91219d26ae1a4f6bd53d176
Angeline Teets <angie@angiemac.lan>
2020-12-10 23:23:31 -0800

    create view

--- movies_app/views.py
+++ movies_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.views.generic import ListView, CreateView
 
 
-# movies = [
-#   Movie('Matrix', 1999, 'Wachowski', 'Sci-Fi'),
-#   Movie('Klaus', 2019, 'Sergio Pablos', 'Holiday'),
-#   Movie('Toy Story', 1995, 'John Lassiter', 'Family')
-# ]
+class MovieList(ListView):
+    model = Movie
+    template_name = 'movies/index.html'
+
+
+class MovieCreate(CreateView):
+    model = Movie
+    fields = '__all__'
 
 def home(request):
     return HttpResponse('<h1>Hello</h1>')
@@ -15,9 +19,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def movies_index(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/index.html', {'movies': movies})
+# def movies_index(request):
+#     movies = Movie.objects.all()
+#     return render(request, 'movies/index.html', {'movies': movies})
 
 def movies_detail(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
